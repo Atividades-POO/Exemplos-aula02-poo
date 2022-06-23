d22a35f799d35670f63b721a6100c4fe9f764cb1
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-23 10:01:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -48,3 +48,15 @@ n1 = N(10, 20) # cria um objeto da classe numero com valores 10 e 20
 n1.imprime() # vai imprimir "valor1 = 10, valor2 = 20"
 # fazer a soma dos dois números
 print(f"a soma = {n1.num1 + n1.num2}") # vai imprimir "a soma = 30"
+
+# 4 - importar a classe DepartamentoPessoal do ex02 e crie um alias para ela
+from ex02 import DepartamentoPessoal as DP
+# 4.1 crie uma instância da classe DepartamentoPessoal
+dp1 = DP('david', 'vendedor', '123456789', 1000)
+#  chame o método imprime da instância criada
+dp1.imprime() # vai imprimir "nome = david, cargo = vendedor, cpf = 123456789, salário = 1000"
+#  altere o nome da pessoa
+dp1.nome = "joão" # altera o nome da pessoa
+#  chame o método imprime da instância criada
+dp1.imprime() # vai imprimir "nome = joão, cargo = vendedor, cpf = 123456789, salário = 1000"
+
