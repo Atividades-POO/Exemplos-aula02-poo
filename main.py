0da35930477ca96394c68239aa8fc9d2ee904763
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-23 09:12:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 #
 # 1 - importar a classe vazia e crie um alias para ela
 from classevazia import ClasseVazia as CV
-
 # 1.1 crie uma instância da classe vazia
 cv1 = CV()
+
+# 2  importar a classe pessoa e crie um alias para ela
+from pessoa import Pessoa as P
+# 2.1 crie uma instância da classe pessoa
+p1 = P()
+p2 = P()
+p3 = P()
+print(p1, p2, p3) # vai imprimir tres objetos da classe pessoa, com respectivos endereços de memória
+# imprima o nome da pessoa 1
+print(p1.nome)
+# imprima o nome da pessoa 2
+print(p2.nome)
+# imprima o nome da pessoa 3
+print(p3.nome)
+
+# altere o nome da pessoa 1
+p1.nome = "João" # altera o nome da pessoa 1
+# altere o nome da pessoa 2
+p2.nome = "Maria" # altera o nome da pessoa 2
+# altere o nome da pessoa 3
+p3.nome = "Pedro" # altera o nome da pessoa 3
+
+# imprima o nome da pessoa 1
+print(p1.nome) # vai imprimir "João"
+# imprima o nome da pessoa 2
+print(p2.nome) # vai imprimir "Maria"
+# imprima o nome da pessoa 3
+print(p3.nome) # vai imprimir "Pedro"
+
