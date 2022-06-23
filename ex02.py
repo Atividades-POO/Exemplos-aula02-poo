80c9d8b8eeee0de0c4ccb10d03b58035b959e6e6
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-23 09:47:18 -0300

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -2,11 +2,13 @@
 # Crie uma classe para o
 # departamento pessoal.
 class DepartamentoPessoal:
+  
   def __init__(self, nome, cargos, CPF, salario):
     self.nome = nome
     self.cargos = cargos
     self.CPF = CPF
     self.salario = salario
+
   def imprime(self):
     print(f"Nome: {self.nome}")
     print(f"Cargos: {self.cargos}")
