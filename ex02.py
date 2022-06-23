3fd0232896ccc4923fcf9dd288886af73146d828
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-23 10:04:31 -0300

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -3,14 +3,14 @@
 # departamento pessoal.
 class DepartamentoPessoal:
 
-  def __init__(self, nome, cargo, CPF, salario):
-    self.nome = nome
+  def __init__(self, nome, cargo, CPF, salario): # inicializador
+    self.nome = nome # atributo
     self.cargos = cargo
     self.CPF = CPF
     self.salario = salario
 
-  def imprime(self):
-    print(f"Nome: {self.nome}")
+  def imprime(self): # método de instância
+    print(f"Nome: {self.nome}") 
     print(f"Cargos: {self.cargos}")
     print(f"CPF: {self.CPF}")
-    print(f"Salário: {self.salario}")
+    print(f"Salário: {self.salario}") 
