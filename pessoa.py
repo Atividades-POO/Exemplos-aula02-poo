bcd126dc0396c4135c87e7dba3e34da7e435699c
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-23 09:03:52 -0300

    Create pessoa.py

---
+++ pessoa.py
@@ -0,0 +1,9 @@
+#
+# 2 - crie uma classe pessoa com o atributo nome
+# e instancie um objeto dessa classe no arquivo principal.
+#
+class Pessoa:
+  nome = "padrão"
+
+  def olaMundo(self):
+    print("Olá Mundo do POO!")
