42989560077ee909f1c4da0d768aba29c1e2bc4f
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-26 14:26:07 -0300

    Update pessoa.py

--- pessoa.py
+++ pessoa.py
@@ -1,4 +1,11 @@
+# Autores:
+# Michel Silva
+# Emanuel Frank
+# Carlos Eduardo
+# data: 26/06/2022
 #
+#
+
 # 2 - crie uma classe pessoa com o atributo nome
 # e instancie um objeto dessa classe no arquivo principal.
 #
